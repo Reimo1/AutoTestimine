787d68e99d4842888dac935920d60e1a0d4cfcce
Reimo1 <reimoloopere@gmail.com>
2017-09-24 23:37:55 +0300

    project

---
+++ src/test/java/Test.java
@@ -0,0 +1,36 @@
+public class Test {
+    @Test
+    public void connection(){
+
+    }
+
+    @Test
+    public void maxDegreeHigherThanMinDegree(){
+
+    }
+
+    @Test
+    public void currentTemperatureValid() {
+
+    }
+
+    @Test
+    public void threeDayMaxTempValid() {
+
+    }
+
+    @Test
+    public void threeDayMinTempValid(){
+
+    }
+
+    @Test
+    public void threeDayForecastValid(){
+
+    }
+
+    @Test
+    public void coordinatesInCorrectFormat(){
+
+    }
+}
